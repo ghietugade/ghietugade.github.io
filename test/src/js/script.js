eadd0ea77ebee3e4df5b975406a209c4b85e6c7b
Angie Tugade <angie.tugade@novafabrik.com>
2017-03-06 16:18:38 +0800

    update test site

--- test/src/js/script.js
+++ test/src/js/script.js
@@ -1,19 +1,21 @@
-var SizingGuide = (function(){
+var SizeGuideSlider = (function(){
   var $slider = $(".m-sizing-guide__slider"),
       $sliderWrap = $(".m-sizing-guide__slider-wrap"),
       $sliderItem = $(".m-sizing-guide__slider-item");
+      $sliderFirstItem = $(".m-sizing-guide__slider-item:first");
 
   var totalWidth = 0;
-  
+  $sliderItem.width($(window).width());
+
   $sliderItem.each(function(){
-      totalWidth = totalWidth + $(this).outerWidth(true);
+    totalWidth = totalWidth + $(this).outerWidth(true);
   });
   
   var maxScrollPosition = totalWidth - $sliderWrap.outerWidth();
-  
+
   $slider.width(totalWidth);
 
-  $(".m-sizing-guide__slider-item:first").addClass("slider-item--active");
+  $sliderFirstItem.addClass("slider-item--active");
 
   var isMobile = function(){
       if(window.innerWidth <= 760) {
@@ -43,22 +45,43 @@ var SizingGuide = (function(){
       };
   };
 
-  var clickPrev = function(event) {  
-      if(isMobile()){
-        var $targetItem = $(".slider-item--active").prev();
+  var isLastSlide = function(){
+      var lastPosition =  -maxScrollPosition;
+      if ($slider.css('left') <= lastPosition + "px"){
+          return true;
       }else{
-        var $targetItem = $(".slider-item--active").prev().prev().prev();
+          return false;
+      }
+  }
+
+  var clickPrev = function(event) {  
+      var $itemActive = $(".slider-item--active");
+
+      if(isMobile()){
+        var $targetItem = $itemActive.prev();
+      }else{
+        var $targetItem = $itemActive.prev().prev().prev();
       }
       toSliderItem($targetItem);
   };
 
   var clickNext = function(event) {
-      if(isMobile()){
-        var $targetItem = $(".slider-item--active").next();
+      var $itemActive = $(".slider-item--active");
+
+      if (isLastSlide()){
+          $itemActive.removeClass("slider-item--active");
+          $sliderFirstItem.addClass("slider-item--active");
+          $slider.animate({
+              left : 0
+          });
       }else{
-        var $targetItem = $(".slider-item--active").next().next().next();
+          if(isMobile()){
+            var $targetItem = $itemActive.next();
+          }else{
+            var $targetItem = $itemActive.next().next().next();
+          }
+          toSliderItem($targetItem);
       }
-      toSliderItem($targetItem);
   };
 
   var bindFunctions = function() {
@@ -72,9 +95,10 @@ var SizingGuide = (function(){
 
   return {
       init: init
+      
   };
 })();          
 
-$(window).load(function(){
-  SizingGuide.init();
+$(window).ready(function(){
+  SizeGuideSlider.init();
 });
